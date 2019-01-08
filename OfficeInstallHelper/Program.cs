bd36b18edf14137d64cd85a4aee07f37bbd51493
Justin Trantham <takescake@outlook.com>
2019-01-08 03:18:49 -0700

    Add project files.

---
+++ OfficeInstallHelper/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeInstallHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
